11e93d26178dcc20c2fb140ba34f1ce5089108fb
Grahame White <grahame.white@apollo-fire.com>
2024-07-06 15:19:33 +0100

    Simplified connecting LogicArrays together

--- src/libLogica/IO/LogicArray.cs
+++ src/libLogica/IO/LogicArray.cs
@@ -16,4 +16,24 @@ public class LogicArray<T> where T : IInputOutput, new()
     public IInputOutput this[Int32 index] => _bits[index];
 
     public Int32 Count => _bits.Length;
+
+    public void Connect<TInputOutput>(LogicArray<TInputOutput> sources) where TInputOutput : IInputOutput, new()
+    {
+        // Generic needs to be different to T to allow for IO of different type to be passed in as an argument
+        // for example if the function required LogicArray<T> then
+        // <input>.connect<input>  would be valid, but
+        // <input>.connect<output> would not be valid
+        for (var i = 0; i < Count; i++)
+        {
+            Int32 i1 = i;
+            sources[i].SignalChanged += (o, e) => _bits[i1].Value = e.Value;
+        }
+    }
+
+    public IEnumerable<Boolean> GetValues()
+    {
+        List<Boolean> result = [];
+        result.AddRange(_bits.Select(bit => bit.Value));
+        return result;
+    }
 }
