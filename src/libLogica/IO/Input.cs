858730b4b21a44d5021ca37ffa7a0c43419dc2e5
Grahame White <grahame.white@apollo-fire.com>
2024-07-06 15:25:59 +0100

    Removed duplication in Input and Output classes

--- src/libLogica/IO/Input.cs
+++ src/libLogica/IO/Input.cs
@@ -1,28 +1,3 @@
 ﻿namespace LibLogica.IO;
 
-public class Input : IInputOutput
-{
-    private Boolean _value;
-
-    public event EventHandler<SignalChangedArgs> SignalChanged = delegate { };
-
-    public Boolean Value
-    {
-        get => _value;
-        set
-        {
-            if (Value == value) return;
-            _value = value;
-            SignalChanged(this, new SignalChangedArgs(value));
-        }
-    }
-
-    public void Connect(IInputOutput source)
-    {
-        // Set to current source value
-        Value = source.Value;
-
-        // Monitor for any future changes
-        source.SignalChanged += (o, e) => Value = e.Value;
-    }
-}
+public class Input : InputOutput;
