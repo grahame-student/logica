56025e27352fde45d8950eaf7992e864ca3dd58d
Grahame White <mrloquax@googlemail.com>
2025-07-21 01:28:28 +0100

    Added 1bit x 8 RAM block

--- src/libLogica/Blocks/Memory/Ram1x8.cs
+++ src/libLogica/Blocks/Memory/Ram1x8.cs
@@ -1,5 +1,62 @@
+using System;
+using System.Collections.Generic;
+using LibLogica.Gates;
+using LibLogica.IO;
+
 namespace LibLogica.Blocks.Memory;
 
-internal class Ram1x8
+public class Ram1x8 : LogicElement
 {
+    private readonly Decoder3to8 _decoder;
+    private readonly Memory1x8 _memory;
+    private readonly Selector8to1 _selector;
+
+    // Inputs
+    public LogicArray<Input> Address { get; }
+    public Input Write { get; } = new();
+    public Input DataIn { get; } = new();
+
+    // Outputs
+    public Output DataOut { get; } = new();
+
+    public Ram1x8()
+    {
+        _decoder = new Decoder3to8();
+        _memory = new Memory1x8();
+        _selector = new Selector8to1();
+        Address = new LogicArray<Input>(3);
+
+        for (Int32 i = 0; i < 3; i++)
+        {
+            _decoder.Address[i].Connect(Address[i]);
+            _selector.Address[i].Connect(Address[i]);
+        }
+        _decoder.Write.Connect(Write);
+        _memory.DataIn.Connect(DataIn);
+        _memory.Write.Connect(_decoder.Output);
+        _selector.Input.Connect(_memory.DataOut);
+        DataOut.Connect(_selector.Output);
+    }
+
+    public override void Update()
+    {
+        _decoder.Update();
+        _memory.Update();
+        _selector.Update();
+    }
+
+    protected (IEnumerable<String> ids, IEnumerable<Boolean> values) BuildDebugInfo() =>
+        DebugInfo()
+            .AddLocal(nameof(DataIn), DataIn)
+            .AddLocal(nameof(Write), Write)
+            .AddArray(nameof(Address), Address)
+            .AddLocal(nameof(DataOut), DataOut)
+            .AddChildren(_decoder)
+            .AddChildren(_memory)
+            .AddChildren(_selector)
+            .Build();
+
+    public override IEnumerable<String> GetIds() => BuildDebugInfo().ids;
+
+    public override IEnumerable<Boolean> GetValues() => BuildDebugInfo().values;
 }
